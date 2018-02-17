db42d2e217c19d4555cb71b349b1b81cd9026994
Vladislav Smirnov <vdmsmirnov@gmail.com>
2018-02-17 23:39:37 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,222 @@
+
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = (options = { env: 'production' }) => {
+    const PRODUCTION = options.env === 'production';
+    const TEST_COVERAGE = options.env === 'test-coverage';
+    const TEST = options.env === 'test' || TEST_COVERAGE;
+    const UGLIFY = options.uglify || false;
+
+    const FONT_LIMIT = PRODUCTION ? 10000 : 1000000;
+    const GRAPHICS_LIMIT = PRODUCTION ? 10000 : 1000000;
+
+    const webpackConfig = {
+        cache: true,
+        devtool: TEST ? 'inline-source-map' : 'source-map',
+        module: {
+            rules: [{
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: [
+                    pathResolver.node_modules()
+                ],
+                options: {
+                    presets: ['flow', 'env']
+                }
+            }, {
+                test: /\.js$/,
+                loader: 'eslint-loader',
+                exclude: [
+                    pathResolver.compiled(),
+                    pathResolver.node_modules(),
+                    pathResolver.tests()
+                ],
+                options: {
+                    failOnError: true
+                }
+            }, {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [{
+                        loader: 'css-loader'
+                    }, {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                            plugins: () => {
+                                const plugins = [
+                                    autoprefixer({
+                                        browsers: ['last 2 versions']
+                                    })];
+                                if (UGLIFY) {
+                                    plugins.push(cssnano({
+                                        preset: ['default', {
+                                            discardComments: {
+                                                removeAll: true
+                                            }
+                                        }]
+                                    }));
+                                }
+                                return plugins;
+                            }
+                        }
+                    }]
+                })
+            }, {
+                test: /\.html$/,
+                loader: 'html-loader'
+            }, {
+                test: /\.woff(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    prefix: 'fonts/',
+                    name: '[path][name].[ext]',
+                    limit: FONT_LIMIT,
+                    mimetype: 'application/font-woff'
+                }
+            }, {
+                test: /\.eot(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    prefix: 'fonts/',
+                    name: '[path][name].[ext]',
+                    limit: FONT_LIMIT,
+                    mimetype: 'application/font-eot'
+                }
+            }, {
+                test: /\.ttf(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    prefix: 'fonts/',
+                    name: '[path][name].[ext]',
+                    limit: FONT_LIMIT,
+                    mimetype: 'application/font-ttf'
+                }
+            }, {
+                test: /\.woff2(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    prefix: 'fonts/',
+                    name: '[path][name].[ext]',
+                    limit: FONT_LIMIT,
+                    mimetype: 'application/font-woff2'
+                }
+            }, {
+                test: /\.otf(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    prefix: 'fonts/',
+                    name: '[path][name].[ext]',
+                    limit: FONT_LIMIT,
+                    mimetype: 'font/opentype'
+                }
+            }, {
+                test: /\.svg(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    prefix: 'fonts/',
+                    name: '[path][name].[ext]',
+                    limit: GRAPHICS_LIMIT,
+                    mimetype: 'image/svg+xml'
+                }
+            }, {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: GRAPHICS_LIMIT,
+                }
+            }]
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': PRODUCTION ? '"production"' : '"development"',
+                __DEV__: !PRODUCTION
+            }),
+            new ExtractTextPlugin({
+                filename: UGLIFY ? cssFileNameMin : cssFileName
+            }),
+            new webpack.optimize.ModuleConcatenationPlugin()
+        ],
+        resolve: {
+            modules: [
+                pathResolver.source(),
+                pathResolver.node_modules()
+            ]
+        },
+        devServer: {
+            noInfo: true,
+            stats: 'minimal',
+            port: 3000
+        }
+    };
+
+    if (!TEST) {
+        webpackConfig.entry = ['babel-polyfill', pathResolver.source('coreApi.js')];
+        webpackConfig.output = {
+            path: pathResolver.compiled(),
+            filename: jsFileName,
+            library: 'core',
+            libraryTarget: 'umd'
+        };
+        if (options.clean !== false) {
+            webpackConfig.plugins.push(
+                new CleanWebpackPlugin([ pathResolver.compiled() ], {
+                    root: pathResolver.root(),
+                    verbose: false,
+                    exclude: ['localization']
+                })
+            );
+        }
+    }
+    if (TEST_COVERAGE) {
+        webpackConfig.module.rules.push({
+            test: /\.js$|\.jsx$/,
+            enforce: 'post',
+            exclude: [
+                pathResolver.tests(),
+                pathResolver.node_modules()
+            ],
+            use: {
+                loader: 'istanbul-instrumenter-loader',
+                options: { esModules: true }
+            },
+        });
+    }
+
+    if (PRODUCTION) {
+        webpackConfig.output.filename = UGLIFY ? jsFileNameMin : jsFileName;
+        webpackConfig.devtool = 'source-map';
+
+        webpackConfig.plugins.push(
+            new webpack.optimize.OccurrenceOrderPlugin()
+        );
+
+        if (UGLIFY) {
+            webpackConfig.plugins.push(
+                new webpack.optimize.UglifyJsPlugin({
+                    uglifyOptions: {
+                        compress: {
+                            warnings: true,
+                            dead_code: true,
+                            properties: true,
+                            conditionals: true,
+                            evaluate: true,
+                            comparisons: true
+                        }
+                    },
+                    sourceMap: true,
+                    parallel: true,
+                    output: {
+                        comments: false
+                    }
+                }));
+        }
+    }
+
+    return webpackConfig;
+};
